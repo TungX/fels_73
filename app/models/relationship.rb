b974b5ec0dab0934b290502670bde92837728de9
Yin Long <blue.rose.hut@gmail.com>
2015-09-01 13:45:11 +0700

    Show activity

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -3,4 +3,15 @@ class Relationship < ActiveRecord::Base
   belongs_to :followed, class_name: "User"
   validates :follower_id, presence: true
   validates :followed_id, presence: true
+
+  before_create :create_activity
+  before_destroy :update_activity
+
+  def create_activity
+    follower.activities.create state: :follow, target_id: followed.id
+  end
+
+  def update_activity
+    follower.activities.create state: :unfollow, target_id: followed.id
+  end
 end
